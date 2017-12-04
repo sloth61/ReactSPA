c152206125df9e765ebb05677631e63900738940
sloth61 <33154710+sloth61@users.noreply.github.com>
2017-12-04 18:55:40 +0200

    -fix modal window for edge and chrome

--- src/Main.js
+++ src/Main.js
@@ -23,33 +23,33 @@ const customStyles = {
     backgroundColor   : 'rgba(255, 255, 255, 0)'
   },
   content : {
-  
+
     WebkitOverflowScrolling    : 'touch',
-   
+
   }
 };
- 
+
 
 
 class Main extends Component {
 	constructor() {
     super();
- 
+
     this.state = {
       modalIsOpen: false
     };
- 
+
     this.openModal = this.openModal.bind(this);
-    
+
     this.closeModal = this.closeModal.bind(this);
   }
- 
+
   openModal() {
     this.setState({modalIsOpen: true});
   }
- 
- 
- 
+
+
+
   closeModal() {
     this.setState({modalIsOpen: false});
   }
@@ -57,10 +57,10 @@ class Main extends Component {
   render() {
     return (
 	<HashRouter>
-	
+
         <div className="wrapper">
 		<div className="header">
-          
+
 		  </div>
           <ul className="NavBar">
             <li><NavLink exact to="/">Главная</NavLink></li>
@@ -81,22 +81,22 @@ class Main extends Component {
           style={customStyles}
           contentLabel="Example Modal"
         >
- 
+
           <div className="modalheader"><span>Контакты</span><span onClick={this.closeModal} className="close">&times;</span></div>
           <div><p>Email : email@mail.com</p>
       <p>тел. +380000000000</p></div>
       <div className="modalfooter"> </div>
-        </Modal> 
+        </Modal>
           <div className="content">
                  <Route exact path="/" component={Home}/>
             <Route path="/instruction" component={Instruction}/>
             <Route path="/finansi" component={Finansi}/>
 			<Route path="/risk" component={Risk}/>
           </div>
-		  <div className="Aside"> <div className="Asidecont"><a href=""><img src="pexels-photo-533572.jpeg" width="100" height="100"/><span>Туристический бизнес в Швейцарии</span></a>
+		  <div className="Aside"> <div className="Asidecont"><a href=""><img src="" width="100" height="100"/><span>Туристический бизнес в Швейцарии</span></a>
 		  </div>
-		  <div className="Asidecont"><a href=""><img src="pexels-photo-533572.jpeg" width="100" height="100"/><span>Туристический бизнес в Швейцарии</span></a></div>
-		  <div className="Asidecont"><a href=""><img src="pexels-photo-533572.jpeg" width="100" height="100"/><span>Туристический бизнес в Швейцарии</span></a></div>
+		  <div className="Asidecont"><a href=""><img src="" width="100" height="100"/><span>Туристический бизнес в Швейцарии</span></a></div>
+		  <div className="Asidecont"><a href=""><img src="" width="100" height="100"/><span>Туристический бизнес в Швейцарии</span></a></div>
 		  </div>
 		  <div className="footer"><span>Обращайтесь за консультациями в нашу фирму, предоставим квалифицированное юридическое сопровождение всех нужных процессов.</span></div>
         </div>
